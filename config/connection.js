104ba245984aec4337ab2ccc078705a84209b551
tyleryonke <tyleryonke@gmail.com>
2017-01-24 12:28:58 -0600

    added changes for upload

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,17 @@ var config = {
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Timothy92",
+  password: "PASSWORD GOES HERE",
   database: "burgers_db"
 };
 
-var connection = mysql.createConnection(config);
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  var connection = mysql.createConnection(config);
+}
+
+
 
 module.exports = connection;
